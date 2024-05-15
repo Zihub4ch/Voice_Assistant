51c867b502b3e092ef3411c570d34a3638435060
CustOmnRer <ANDREY-YAKOVENKO90@mail.ru>
2024-05-15 23:57:51 +0300

    Version_1.2

--- main/INTERFACE.py
+++ main/INTERFACE.py
@@ -13,9 +13,24 @@ window.geometry("1024x720")
 window["bg"] = "black"
 
 activate = tk.Label(window, text = "Включить голосового помощника", font = ("Arial Bold", 15), fg = "orange", bg = "black")
-activate.place(x = 360, y = 30)
+activate.place(x = 385, y = 30)
+
+btn = tk.Button(window, text = "Включить", width = "50", height = "2", fg = "black", bg = "white")
+btn.place(x = 360, y = 80)
+
+hot_buttons = tk.Label(window, text = "Ввести горячую клавишу", font = ("Arial Bold", 15), fg = "orange", bg = "black")
+hot_buttons.place(x = 420, y = 200)
 
 Entertext = tk.Entry(fg = "black", width = 50)
-Entertext.place(x = 360, y = 480)
+Entertext.place(x = 385, y = 240)
+
+btn = tk.Button(window, text = "Добавить", width = "50", height = "2", fg = "black", bg = "white")
+btn.place(x = 360, y = 280)
+
+list = tk.Label(window, text = "Список горячих клавиш", font = ("Arial Bold", 15), fg = "orange", bg = "black")
+list.place(x = 432, y = 360)
+
+btn = tk.Button(window, text = "Открыть", width = "50", height = "2", fg = "black", bg = "white")
+btn.place(x = 360, y = 390)
 
 window.mainloop()
