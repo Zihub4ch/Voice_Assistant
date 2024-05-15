72e17cc55278870d58875a56ecf763c14293ec68
CustOmnRer <ANDREY-YAKOVENKO90@mail.ru>
2024-05-15 23:34:43 +0300

    First_Version

--- main/INTERFACE.py
+++ main/INTERFACE.py
@@ -1,1 +1,21 @@
-INTERFACE
+import tkinter as tk
+from random import randint, choice
+from tkinter import messagebox
+
+window = tk.Tk()
+
+window.resizable(width = False, height = False)
+
+window.title("Голосовой помощник")
+
+window.geometry("1024x720")
+
+window["bg"] = "black"
+
+activate = tk.Label(window, text = "Включить голосового помощника", font = ("Arial Bold", 15), fg = "orange", bg = "black")
+activate.place(x = 360, y = 30)
+
+Entertext = tk.Entry(fg = "black", width = 50)
+Entertext.place(x = 220, y = 690)
+
+window.mainloop()
