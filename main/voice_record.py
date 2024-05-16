55d69efbfd94a55041660a81b4b4e43564b00cc9
Zihub4ch <zihub.herimeri@yandex.ru>
2024-05-16 19:40:14 +0300

    beta_ver_1

--- main/voice_record.py
+++ main/voice_record.py
@@ -3,6 +3,7 @@ import os
 import pyaudio
 import re
 import keyboard
+import time
 
 model = Model(r"C:/Users/User/Desktop/PROG/.vscode/voice_helper/vosk-model-small-ru-0.22") # полный путь к модели
 rec = KaldiRecognizer(model, 16000)
@@ -18,16 +19,32 @@ stream = p.open(
 )
 stream.start_stream()
 
+
 def Record():
-    b = 0
-    while  b < 5:
+    while True:
         data = stream.read(16000)
         if len(data) == 0:
             break
+        # print(rec.Result() if rec.AcceptWaveform(data) else "Услышал: ", re.sub('[{|}|partial|"|:]', '', rec.PartialResult()))
+        # print("Получил: ", re.sub('[{|}|text|"|:]', '', rec.FinalResult()))
+        text = rec.Result() if rec.AcceptWaveform(data) else "Услышал: ", re.sub('[{|}|partial|"|:]', '', rec.PartialResult())  
+        return(text)
+    
+def commands():
+    while True:
+        text = list(Record())
+        text_to_str = ' '.join(text)
+        text_filt = re.sub('[{|}|text|"|:]', '', text_to_str) 
+        print("Получил: ", text_filt)
+        
+        if "скрин" in text_filt:
+            keyboard.press('win+shift+s')
+            time.sleep(0.1)
+            keyboard.release('win+shift+s')
+        
+        elif "поставь" in text_filt:
+            keyboard.press('ctrl+v')
+            time.sleep(0.05)
+            keyboard.release('ctrl+v')
 
-        print(rec.Result() if rec.AcceptWaveform(data) else "Услышал: ", re.sub('[{|}|partial|"|:]', '', rec.PartialResult()))
-
-        print("Получил: ", re.sub('[{|}|text|"|:]', '', rec.FinalResult()))
-        b += 1
-    return()
-
+commands() 
