46129f3428df1332bd6891a51d14ca52a7837488
CustOmnRer <ANDREY-YAKOVENKO90@mail.ru>
2024-06-26 17:01:40 +0300

    Cool_InterFACE_v0.1.3

--- Cool_InterFACE/main.py
+++ Cool_InterFACE/main.py
@@ -1,15 +1,11 @@
-# This is a sample Python script.
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+Form, Window = uic.loadUiType("VA.ui")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutteif __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app = QApplication([])
+window = Window()
+form = Form()
+form.setupUi(window)
+window.show()
+app.exec()
